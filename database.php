cc9ba91550839f7d9896849862ea751d585512d3
Niels Doorn <nielsdoorn@gmail.com>
2014-01-07 13:56:09 +0100

    Update database.php
    updated to use mysqli

--- database.php
+++ database.php
@@ -7,12 +7,15 @@ $databasenaam = "gastenboek";
 
 
 // maak verbinding met de database
-mysql_connect($databaseserver, $gebruikersnaam, $wachtwoord);
+$mysqli = new mysqli($databaseserver, $gebruikersnaam, $wachtwoord, $databasenaam);
 
-// selecteer een database
-mysql_select_db($databasenaam);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
 
 function maakTabelAan() {
+	global $mysqli;
 	$maakTabelAan = "
 		CREATE TABLE IF NOT EXISTS `gastenboek` (
 			`id` INT NOT NULL AUTO_INCREMENT, 
@@ -23,7 +26,7 @@ function maakTabelAan() {
 			`tekst` TEXT NOT NULL, 
 			PRIMARY KEY (`id`)
 		) ENGINE = MyISAM";
-	mysql_query($maakTabelAan);
+	$mysqli->query($maakTabelAan);
 }
 
 function voegTestDataToe() {
@@ -31,19 +34,22 @@ function voegTestDataToe() {
 }
 
 function haalBerichtenOp() {
+	global $mysqli;
 	$berichten = array();
 	$sql = "SELECT * FROM gastenboek ORDER BY datum DESC";
-	$resultaat = mysql_query($sql);
-	while($bericht = mysql_fetch_assoc($resultaat)) {
+	$resultaat = $mysqli->query($sql);
+	$resultaat->data_seek(0);
+	while($bericht = $resultaat->fetch_assoc()) {
 		$berichten[] = $bericht;
 	}
 	return $berichten;
 }
 
 function voegBerichtToe($titel, $naam, $email, $tekst) {
+	global $mysqli;
 	$insertQuery = "INSERT INTO `gastenboek` (titel, naam, email, tekst) VALUES ('%s', '%s', '%s', '%s');";
 	$sql = sprintf($insertQuery, $titel, $naam, $email, $tekst);
-	mysql_query($sql);
+	$mysqli->query($sql);
 }
 
 ?>
